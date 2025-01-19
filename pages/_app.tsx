bc35f1ff00a95ee7a6fc0312d36e4a67a4c29548
Kenneth Okoronkwo <111024479+kenchigozie23@users.noreply.github.com>
2025-01-19 08:58:46 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,37 @@
-import {Provider} from 'react-redux';
-import {store} from '../redux/store';
-import 'nprogress/nprogress.css';
-import '../styles/styles.scss';
+import {SessionProvider} from 'next-auth/react'
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
+import "nprogress/nprogress.css";
+import "../styles/styles.scss";
 
-import 'swiper/scss';
-import 'swiper/scss/navigation';
-import 'swiper/scss/pagination';
-import 'swiper/scss/scrollbar';
-import 'animate.css';
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import "swiper/scss/scrollbar";
+import "animate.css";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import 'boundless-checkout-react/dist/index.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "boundless-checkout-react/dist/index.css";
 
-import '@fortawesome/fontawesome-free/css/svg-with-js.css';
+import "@fortawesome/fontawesome-free/css/svg-with-js.css";
 
-import {AppProps} from 'next/app';
-import RouterListener from '../components/RouterListener';
-import LoadingLine from '../components/LoadingLine';
+import { AppProps } from "next/app";
+import RouterListener from "../components/RouterListener";
+import LoadingLine from "../components/LoadingLine";
+import { Session } from 'inspector';
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps: {session, ...pageProps } }: AppProps) {
 	return (
+			<SessionProvider session={session}>
 		<Provider store={store}>
 			<RouterListener />
 			<LoadingLine />
-			<Component {...pageProps} />
+				<Component {...pageProps} />
 		</Provider>
+		</SessionProvider>
 	);
 }
 
